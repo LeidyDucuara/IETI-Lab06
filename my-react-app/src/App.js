3b636939acc3c2ce100ff59d16ac6a9cca6c57c4
LeidyDucuara <leidy.ducuara@mail.escuelaing.edu.co>
2022-03-10 23:59:52 -0500

    part3

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path = '/login' element = {<Login/>}/>
+        <Route path = '/' element = {<Login/>}/>
         <Route path = '/inicio' element = {<Inicio/>}/>
       </Routes>
     </Router>
--- my-react-app/src/components/Inicio.js
+++ my-react-app/src/components/Inicio.js
@@ -1,10 +1,32 @@
-import React from "react";
+import {useState, useEffect} from 'react'
 
-const inicio=()=>{
-    return(
-        <h1>
-            Inicio
-        </h1>
+const Task = () => {
+
+	const [task, setTask] = useState('');
+	const [error, setError] = useState('');
+        
+	useEffect(() => {
+        const requestOptions = {
+            mode: 'cors',
+            method: 'GET',
+            headers:{'Content-Type': 'application/json'}
+        }
+
+		const fetchResource = async() => {
+            try{
+            let res = await fetch('http://localhost:8080/v1/tasks')
+            console.log(res.statusText)
+            let data = await res.json()
+            setTask(data)
+            }catch(error){
+                setError(error)
+            }
+        } 
+        fetchResource()
+    })
+    return (
+        <h1>Inicio</h1>
     )
 }
-export default inicio
+
+export default Task
